a8e4a623001a5dbb4ec2ed28c3b2a2717ca48009
davidosi93 <d.osipov93@googlemail.com>
2023-07-20 19:46:41 +0200

    Icon is left

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { slideInUpOnEnterAnimation, slideOutDownOnLeaveAnimation } from 'angular-animations';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
+  animations: [
+    slideInUpOnEnterAnimation({ duration: 300 }),
+    slideOutDownOnLeaveAnimation({ duration: 500 })
+  ]
 })
 export class FooterComponent {
+
+  isImprintOpen: boolean = false;
+
+  toggleImprint() {
+    this.isImprintOpen = !this.isImprintOpen;
+  }
 
 }
