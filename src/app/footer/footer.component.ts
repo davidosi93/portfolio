cc8fb347fe5f6fccbcedcc8b58588b890bcb2a32
davidosi93 <d.osipov93@googlemail.com>
2024-02-08 15:30:01 +0100

    finish

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 import { slideInUpOnEnterAnimation, slideOutDownOnLeaveAnimation } from 'angular-animations';
 
 @Component({
@@ -14,7 +14,21 @@ export class FooterComponent {
 
   isImprintOpen: boolean = false;
 
+  constructor(
+    private renderer: Renderer2,
+    private el: ElementRef
+  ) { }
+
   toggleImprint() {
     this.isImprintOpen = !this.isImprintOpen;
+    this.checkScroll();
+  }
+
+  checkScroll() {
+    if (this.isImprintOpen) {
+      this.renderer.setStyle(document.body, 'overflow', 'hidden');
+    } else {
+      this.renderer.removeStyle(document.body, 'overflow');
+    }
   }
 }
